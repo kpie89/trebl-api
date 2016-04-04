7674710a36baeec3d11d5d85f0ab8d82ed7a5313
kpie89 <kpie89@gmail.com>
2016-04-04 15:08:11 -0400

    finalize playlists controller

---
+++ app/controllers/playlists_controller.rb
@@ -0,0 +1,61 @@
+class PlaylistsController < ProtectedController
+  before_action :set_playlist, only: [:show, :update, :destroy]
+
+  # GET /playlists
+  # GET /playlists.json
+  def index
+    @playlists = Playlist.all
+
+    render json: @playlists
+  end
+
+  # GET /playlists/1
+  # GET /playlists/1.json
+  def show
+    render json: @playlist
+  end
+
+  # POST /playlists
+  # POST /playlists.json
+  def create
+    # @song = current_user.playlist.songs.new song_params
+    # @song.playlist_id = @playlist.id
+    @playlist = current_user.playlists.new(playlist_params)
+
+    if @playlist.save
+      render json: @playlist, status: :created, location: @playlist
+    else
+      render json: @playlist.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /playlists/1
+  # PATCH/PUT /playlists/1.json
+  def update
+    @playlist = Playlist.find(params[:id])
+
+    if @playlist.update(playlist_params)
+      head :no_content
+    else
+      render json: @playlist.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /playlists/1
+  # DELETE /playlists/1.json
+  def destroy
+    @playlist.destroy
+
+    head :no_content
+  end
+
+  private
+
+    def set_playlist
+      @playlist = current_user.playlists.find(params[:id])
+    end
+
+    def playlist_params
+      params.require(:playlist).permit(:title, :desc, :person_id)
+    end
+end
